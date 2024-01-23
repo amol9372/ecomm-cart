1fee777a8627676f6544da504d0ea9ccb98e34f4
amolsingh <amolsingh9372@gmail.com>
2024-01-23 09:51:35 -0500

    port change

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -15,4 +15,4 @@ echo "Pulling image and starting container"
 
 docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) public.ecr.aws/j8h8n1c1
 docker pull public.ecr.aws/j8h8n1c1/ecomm-cart
-docker run -it --env-file /home/ec2-user/docker/env_files/ecomm.env -p 8080:8080 -d public.ecr.aws/j8h8n1c1/ecomm-cart
+docker run -it --env-file /home/ec2-user/docker/env_files/ecomm.env -p 8082:8082 -d public.ecr.aws/j8h8n1c1/ecomm-cart
